bc25a55cb7833726d2587d44fe25bca8b5eff3cf
jadesola98 <77394784+jadesola98@users.noreply.github.com>
2024-06-25 11:43:48 +0100

    updates

--- main_scripy.py
+++ main_scripy.py
@@ -7,31 +7,30 @@ from dotenv import load_dotenv
 from requests.exceptions import RequestException
 
 
-import requests
-
 load_dotenv()
 
 API_KEY         =   os.getenv("API_KEY")
 API_HOST        =   os.getenv("API_HOST")
+LEAGUE_ID       =	os.getenv("LEAGUE_ID")
+SEASON			=	os.getenv("SEASON")
 
 url = "https://api-football-v1.p.rapidapi.com/v3/standings"
-
-querystring = {"season":"2020", "league":"39"}
-
+querystring = {"season":SEASON, "league":LEAGUE_ID}
 headers = {
 	"x-rapidapi-key": API_KEY,
 	"x-rapidapi-host": API_HOST
 }
 
-api_response = requests.get(url, headers=headers, params=querystring)
 
-#print(response.json())
+api_response = requests.get(url, headers=headers, params=querystring)
+#print(api_response.json())
 
 standings_data = api_response.json()['response']
+#print(standings_data)
 #print(standings_data[0]['league']['standings'][0])
+
 
 lst = []
 for info in standings_data[0]['league']['standings'][0]:
     lst.append(info)
 print(len(lst))
-
